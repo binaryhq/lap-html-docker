1b70e20525c2ca51ec208c4f48a5babe6f67cb93
ningappa <ningappa@users.noreply.github.com>
2017-06-21 17:50:11 +0530

    Update create_filemanager_users.sh

--- uploads/create_filemanager_users.sh
+++ uploads/create_filemanager_users.sh
@@ -4,9 +4,10 @@ echo '-----------------------'
 echo "filemanager user =>  ${FILEMANAGERUSER:-'testuser'}"
 echo "filemanager pass => ${FILEMANAGERPASSWORD:-'testpassword'}"
 echo '------------------------'
-replace HOSTID ${HOSTID:-'_1'} -- /usr/share/pbn/apache2.conf
-replace FILEMANAGERUSER ${FILEMANAGERUSER:-'testuser'} -- /usr/share/pbn/filemanager/config/.htusers.php
-replace FILEMANAGERPASSWORD $(echo -n ${FILEMANAGERPASSWORD:-'testpassword'} | md5sum | awk '{print $1}') -- /usr/share/pbn/filemanager/config/.htusers.php
+sed -i "s/HOSTID/${HOSTID:-'_1'}/" /usr/share/pbn/apache2.conf
+sed -i "s/FILEMANAGERUSER/${FILEMANAGERUSER:-'testuser'}/" /usr/share/pbn/filemanager/config/.htusers.php
+sed -i "s/FILEMANAGERPASSWORD/$(echo -n ${FILEMANAGERPASSWORD:-'testpassword'} | md5sum | awk '{print $1}')/" /usr/share/pbn/filemanager/config/.htusers.php
+  
 chown -R  www-data:www-data /var/www/html
 
 echo "enjoy!"
